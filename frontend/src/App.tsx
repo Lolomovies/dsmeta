e8180b3fc236db5a27efed05cdcba4ce8db10de9
lorenzo <lorenzomboff@gmail.com>
2022-07-12 00:06:28 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import NotificationButton from "./componentes/NotificationButton/Index"
 
 function App() {
- return(
-  <h1> Olá Mundo!</h1>
- )
+  return (
+    <>
+      <h1> Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
